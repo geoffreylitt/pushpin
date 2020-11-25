d0338678ad65e898cc358945b5e744a3dd678121
Geoffrey Litt <gklitt@gmail.com>
2020-11-25 14:38:41 -0500

    bold text

--- src/renderer/components/content-types/RichTextContent.tsx
+++ src/renderer/components/content-types/RichTextContent.tsx
@@ -1,27 +1,79 @@
-import React, { useState } from 'react'
+// Import React dependencies.
+import React, { useEffect, useMemo, useState, useCallback } from 'react'
+// Import the Slate editor factory.
+import { createEditor, Transforms, Text } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
 import * as ContentTypes from '../../ContentTypes'
 import Content, { ContentProps } from '../Content'
-import { createDocumentLink, HypermergeUrl } from '../../ShareLink'
 import { useDocument } from '../../Hooks'
 import ListItem from '../ui/ListItem'
 import Badge from '../ui/Badge'
 import ContentDragHandle from '../ui/ContentDragHandle'
 import TitleWithSubtitle from '../ui/TitleWithSubtitle'
-import './ThreadContent.css'
+
+// Define a React component to render leaves with bold text.
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
 
 export default function RichText(props: ContentProps) {
-  const [message, setMessage] = useState('')
   const [doc, changeDoc] = useDocument<Doc>(props.hypermergeUrl)
+
+  const editor = useMemo(() => withReact(createEditor()), [])
+  // Add the initial value when setting up our state.
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+
+  // Define a leaf rendering function that is memoized with `useCallback`.
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
 
   if (!doc) {
     return null
   }
 
   return (
-    <div>
-      Hello World
-    </div>
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={newValue => setValue(newValue)}
+    >
+      <Editable
+        renderLeaf={renderLeaf} 
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            case 'b': {
+              console.log("b!")
+              event.preventDefault()
+              Transforms.setNodes(
+                editor,
+                { bold: true },
+                { match: n => Text.isText(n), split: true }
+              )
+              break
+            }
+          }
+        }}/>
+    </Slate>
   )
 }
 
